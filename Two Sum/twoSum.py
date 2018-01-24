91f1114d1a04a39802e4a0d048e197a211d1b039
rdkerr <rogerdkerr@gmail.com>
2018-01-24 16:14:54 -0700

    Finished

--- Two Sum/twoSum.py
+++ Two Sum/twoSum.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python
+#Given an array of integers, return indices of the two numbers such that
+#they add up to a specific target.
+#
+#You may assume that each input would have exactly one solution, and you
+#may not use the same element twice.
+#Example: Given [2,7,11,15], target = 9
+#return 0, 1
 def main():
-    print "HI"
+    testList1 = [2, 7, 11, 15]
+    testTarget1 = 9
+    testList2 = [3, 4, 5, 8]
+    testTarget2 = 12
+    testList3 = [1, 15, 11, 13]
+    testTarget3 = 26
+    twoSum(testList1,testTarget1)
+    twoSum(testList2,testTarget2)
+    twoSum(testList3,testTarget3)
+  
+def twoSum(nums, target):
+    numberMap = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if(nums[i] in numberMap):
+            print(numberMap[nums[i]],i)
+        numberMap[complement] = i
+    
 
 
 if __name__ == "__main__":
