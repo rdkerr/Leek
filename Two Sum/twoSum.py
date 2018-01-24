c394f23f3e8e02940a5223a488b4d3f8c641b0b8
rdkerr <rogerdkerr@gmail.com>
2018-01-24 16:17:07 -0700

    Method returns solution as list instead of printing it

--- Two Sum/twoSum.py
+++ Two Sum/twoSum.py
@@ -13,16 +13,16 @@ def main():
     testTarget2 = 12
     testList3 = [1, 15, 11, 13]
     testTarget3 = 26
-    twoSum(testList1,testTarget1)
-    twoSum(testList2,testTarget2)
-    twoSum(testList3,testTarget3)
+    print(twoSum(testList1,testTarget1))
+    print(twoSum(testList2,testTarget2))
+    print(twoSum(testList3,testTarget3))
   
 def twoSum(nums, target):
     numberMap = {}
     for i in range(len(nums)):
         complement = target - nums[i]
         if(nums[i] in numberMap):
-            print(numberMap[nums[i]],i)
+            return [numberMap[nums[i]],i]
         numberMap[complement] = i
     
 
