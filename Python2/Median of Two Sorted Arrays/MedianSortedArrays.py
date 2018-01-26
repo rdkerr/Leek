048de2230746051414944e033ce658b36e11fa2b
rdkerr <rogerdkerr@gmail.com>
2018-01-26 13:54:30 -0700

    Calculate initial partitions

---
+++ Python2/Median of Two Sorted Arrays/MedianSortedArrays.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#There are two sorted arrays nums1 and nums2 of size m and n respectively.
+#Find the median of the two sorted arrays. The overall run time complexity
+#should be O(log (m+n)).
+
+def main():
+    nums1 = [1,4,7,8]
+    nums2 = [3,6,11,13]
+    print findMedianSortedArrays(nums1,nums2)
+
+def findMedianSortedArrays(nums1, nums2):
+    n = len(nums1)
+    m = len(nums2)
+    front = 0
+    back = n
+    while (front <= back):
+        xPart = (front + back) / 2
+        yPart = (n + m + 1)/2 - xPart
+        print xPart, yPart
+        #Check numbers around partitions
+        if xPart == 0:
+            xLeft = float('-inf')
+        else:
+            xLeft = nums1[xPart - 1]
+        if xPart == 0:
+            xRight = float('inf')
+        else:
+            xRight = nums1[xPart]
+        if yPart == 0:
+            yLeft = float('-inf')
+        else:
+            yLeft = nums2[yPart - 1]
+        if yPart == 0:
+            yRight = float('inf')
+        else:
+            yRight = nums2[yPart]
+        print xLeft, xRight
+        print yLeft, yRight
+        a = raw_input("...")
+            
+        
+    
+    return 0
+    
+if __name__ == "__main__":
+    main()
