2b1a1ba4f38ef4ed050ee92748b4eaf53ea030ca
rdkerr <rogerdkerr@gmail.com>
2018-02-08 11:30:58 -0700

    Finished O(n) solution

--- C++/Longest Substring No Repeat/src/SubstringNoRepeat.cpp
+++ C++/Longest Substring No Repeat/src/SubstringNoRepeat.cpp
@@ -14,6 +14,7 @@
 #include <vector>
 using namespace std;
 
+//O(n^2) time and O(n) space
 int lengthOfLongestSubstring(string s) {
 	int n = s.length();
 	    if (n == 0) return 0;
@@ -30,7 +31,6 @@ int lengthOfLongestSubstring(string s) {
 			}
 			if (j < 0) {
 				T[i] = T[i-1]+1;
-				cout << "IF:" << s[i] << endl;
 				if (T[i] > max ) {
 					max = T[i];
 
@@ -39,7 +39,6 @@ int lengthOfLongestSubstring(string s) {
 			else {
 				T[i] = min(i-j,T[i-1]+1);
 
-				cout << "FF: " << i << " : " << j << " : " << s[i] << endl;
 				if (T[i] > max ) {
 					max = T[i];
 
@@ -49,10 +48,33 @@ int lengthOfLongestSubstring(string s) {
 		return max;
 }
 
+// O(n) space O(128) space
+int lengthOfLongestSubstring2(string s) {
+	int T[128] = {0};
+	int n = s.length();
+	int m = 0;
+	for (int j = 0, i = 0 ; j < n; j++) {
+		i = max(T[int(s[j])],i);
+		m = max(m,j-i+1);
+		T[int(s[j])] = j+1;
+	}
+	return m;
+}
+
+        // try to extend the range [i, j]
+        //for (int j = 0, i = 0; j < n; j++) {
+        //    i = Math.max(index[s.charAt(j)], i);
+        //    ans = Math.max(ans, j - i + 1);
+        //    index[s.charAt(j)] = j + 1;
+        //}
+
+
 int main() {
-	string x = "abba";
+	string x = "c";
 	int y;
 	y = lengthOfLongestSubstring(x);
+	cout << y << endl;
+	y = lengthOfLongestSubstring2(x);
 	cout << y << endl;
 	return 0;
 }
