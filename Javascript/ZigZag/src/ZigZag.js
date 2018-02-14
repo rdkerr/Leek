6049e93b33f10ee12a1b939f0443b9400101d2ab
rdkerr <rogerdkerr@gmail.com>
2018-02-14 12:16:25 -0700

    Completed ZigZag

--- Javascript/ZigZag/src/ZigZag.js
+++ Javascript/ZigZag/src/ZigZag.js
@@ -4,5 +4,24 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
+    if(numRows <= 1) {
+      return s;
+    }
+    var res = "";
+    // Top and bottom rows will not have 2nd element
+    var period = 2 * numRows - 2;
     
+    // O(n) time - each char is only visited once
+    for (var i = 0 ; i < numRows ; i++) {
+      for (var j = i ; j < s.length ; j = j + period) {
+        res = res + s[j];
+        var secondElement = j - i + period - i;
+        if (i > 0 && i < numRows-1) {
+          if (secondElement < s.length) {
+            res = res + s[secondElement];
+          }
+        }
+      }
+    }
+    return res;
 };
