1277f72e1a5a66ce20b9a118c96d10832360e1bd
rdkerr <rogerdkerr@gmail.com>
2018-02-14 23:07:50 -0700

    Completed ReverseInteger

---
+++ C#/ReverseInteger/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReverseInteger {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine(Program.Reverse(-2147483648));
+        }
+        
+        public static int Reverse(int x) {
+        	int res = 0;
+        	int sign = 1;
+        	Console.WriteLine(x);
+        	if (x < 0) {
+        		sign = -1;
+        		x = x * -1;
+        	}
+			if (x == 0 || x == Int32.MinValue) {
+				return 0;
+			}
+			Console.WriteLine(x);
+			while(x >= 10) {
+				Console.WriteLine(res);
+				int last = x % 10;
+				x = x / 10;
+				res = res * 10 + last;
+			}
+			if (Int32.MaxValue/10 < res) {
+				return 0;
+			}
+			else {
+				return (res * 10 + x) * sign;
+			}
+    	}
+    }
+}
