1364fd88482d0b7e1b204bb4238cdf99a07383c1
rdkerr <rogerdkerr@gmail.com>
2018-02-17 23:16:07 -0700

    Init Regular Expression Matching

---
+++ Java/Regular Expression Matching/src/leet/Solution.java
@@ -0,0 +1,29 @@
+package leet;
+
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+ * 
+ * '.' Matches any single character. '*' Matches zero or more of the preceding
+ * element.
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * The function prototype should be: bool isMatch(const char *s, const char *p)
+ * 
+ * Some examples: isMatch("aa","a") - false isMatch("aa","aa") - true
+ * isMatch("aaa","aa") - false isMatch("aa", "a*") - true isMatch("aa", ".*") -
+ * true isMatch("ab", ".*") - true isMatch("aab", "c*a*b") true
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isMatch("a", "a"));
+
+	}
+
+	public static boolean isMatch(String s, String p) {
+		return true;
+	}
+
+}
