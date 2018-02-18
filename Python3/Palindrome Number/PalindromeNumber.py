9991b1154cdbcd7e0fdb48e086a8a275f5ab020a
rdkerr <rogerdkerr@gmail.com>
2018-02-17 20:35:22 -0700

    Init PalindromeNumber

---
+++ Python3/Palindrome Number/PalindromeNumber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#Determine whether an integer is a palindrome. Do this without extra space.
+class Solution:
+    def isPalindrome(self, x):
+        return True
+    
+def main():
+    test = Solution()
+    print(test.isPalindrome(1))
+    print(test.isPalindrome(13))
+    print(test.isPalindrome(131))
+
+
+if __name__ == "__main__":
+    main()
