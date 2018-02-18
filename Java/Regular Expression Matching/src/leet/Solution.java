693729a570bcf2739857c8edc83dbfbc5ec83a63
rdkerr <rogerdkerr@gmail.com>
2018-02-18 15:20:25 -0700

    Passes all test cases

--- Java/Regular Expression Matching/src/leet/Solution.java
+++ Java/Regular Expression Matching/src/leet/Solution.java
@@ -23,6 +23,11 @@ public class Solution {
 	public static boolean isMatch(String s, String p) {
 		boolean [][]T = new boolean[s.length()+1][p.length()+1];
 		T[0][0] = true;
+		for(int j = 1 ; j < p.length() ; j++) {
+			if(p.charAt(j)=='*') {
+				T[0][j+1] = true;
+			}
+		}
 		for(int i = 1 ; i <= s.length() ; i ++) {
 			for(int j = 1 ; j <= p.length() ; j++) {
 			
