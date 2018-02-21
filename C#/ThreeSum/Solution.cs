a2ea7c920628ac6b1cda1a59ad4b563cff0f9352
rdkerr <rogerdkerr@gmail.com>
2018-02-21 16:44:30 -0700

    Iterators not updating properly

--- C#/ThreeSum/Solution.cs
+++ C#/ThreeSum/Solution.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace ThreeSum {
-    public class Solution {
-        public static void Main(string[] args) {
+
+public class Solution {
+	public static void Main(string[] args) {
         	int[] nums = {-1,0,1,2,-1,4};
         	 Console.WriteLine("START");
             Solution.ThreeSum(nums);
@@ -13,13 +13,18 @@ namespace ThreeSum {
         
         public static IList<IList<int>> ThreeSum(int[] nums) {
         	Array.Sort(nums);
+			for(int j = 0 ; j < nums.Length ; j++ ) {
+				Console.WriteLine(nums[j]);
+			}
         	int i = 0;
-        	while (i < nums.Length && nums[i] < 0) {
+        	while (i < nums.Length && nums[i] <= 0) {
         		int complement = 0 - nums[i];
         		int begin = i + 1;
         		int end = nums.Length - 1;
+				Console.WriteLine(complement + " " + begin + " " + end + "\n\n");
         		while (begin < end) {
         			int sum = nums[begin] + nums[end];
+					Console.WriteLine(sum + " " + nums[begin] + " " + nums[end]);
         			if (sum > complement) {
         				begin++;
         			}
@@ -30,8 +35,9 @@ namespace ThreeSum {
         				Console.WriteLine("FOUND");
         			}
         		}
+				i++;
         	}
         	return null;
    		}
-    }
+   	}
 }
