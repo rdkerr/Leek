f5ac1a216cd8087e9ccf97e81abbbf260334c93d
rdkerr <rogerdkerr@gmail.com>
2018-02-21 14:00:06 -0700

    Init 3Sum

---
+++ C#/3Sum/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3Sum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
