a17f8c2935ac5017010396acc995b4ced445d8c4
rdkerr <rogerdkerr@gmail.com>
2018-02-21 16:26:39 -0700

    Refactored to ThreeSum

---
+++ C#/ThreeSum/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ThreeSum {
+    public class Solution {
+        public static void Main(string[] args) {
+        	int[] nums = {-1,0,1,2,-1,4};
+        	 Console.WriteLine("START");
+            Solution.ThreeSum(nums);
+            Console.WriteLine("DONE");
+        }
+        
+        public static IList<IList<int>> ThreeSum(int[] nums) {
+        	Array.Sort(nums);
+        	int i = 0;
+        	while (i < nums.Length && nums[i] < 0) {
+        		int complement = 0 - nums[i];
+        		int begin = i + 1;
+        		int end = nums.Length - 1;
+        		while (begin < end) {
+        			int sum = nums[begin] + nums[end];
+        			if (sum > complement) {
+        				begin++;
+        			}
+        			else if (sum < complement) {
+        				end--;
+        			}
+        			else {
+        				Console.WriteLine("FOUND");
+        			}
+        		}
+        	}
+        	return null;
+   		}
+    }
+}
--- C#/ThreeSum/obj/Debug/netcoreapp2.0/ThreeSum.AssemblyInfo.cs
+++ C#/ThreeSum/obj/Debug/netcoreapp2.0/ThreeSum.AssemblyInfo.cs
@@ -7,10 +7,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("3Sum")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ThreeSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("3Sum")]
-[assembly: System.Reflection.AssemblyTitleAttribute("3Sum")]
+[assembly: System.Reflection.AssemblyProductAttribute("ThreeSum")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ThreeSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
