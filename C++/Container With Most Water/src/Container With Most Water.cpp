276e31fbfcaa467549e6b22b0acb555d6028252c
rdkerr <rogerdkerr@gmail.com>
2018-02-20 22:48:42 -0700

    First attempt

--- C++/Container With Most Water/src/Container With Most Water.cpp
+++ C++/Container With Most Water/src/Container With Most Water.cpp
@@ -7,9 +7,34 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int maxArea(vector<int>& height) {
+	int begin = 0;
+	int end = height.size() - 1;
+	int area = 0;
+	while (begin < end) {
+		int length = min(height[begin], height[end]);
+		int width = end - begin;
+		area = max(area, length*width);
+		cout <<height[begin] << endl;
+		while (height[begin] <= length && begin < end) {
+			begin++;
+		}
+		while (height[end] <= length && begin < end) {
+			end--;
+		}
+
+	}
+    return area;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	vector<int> vec (2,1);
+	int x = maxArea(vec);
+	cout << x << endl; // prints !!!Hello World!!!
+
 	return 0;
 }
+
