605795fac31b67b679c1bb50ec0c1b8af9c8197a
rdkerr <rogerdkerr@gmail.com>
2018-02-21 12:42:58 -0700

    2nd attempt

--- C/Roman To Int/src/RomanToInt.c
+++ C/Roman To Int/src/RomanToInt.c
@@ -12,7 +12,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-int romanToInt(char* s) {
+int getValue(char c) {
+	switch(c) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return 0;
+	}
+}
+
+int romanToInt(char *s) {
+	int res = 0;
+	while(*s) {
+		int current = getValue(s[0]);
+		int next = getValue(s[1]);
+		if (current > next) {
+			res += current;
+		}
+		else {
+			res -= current;
+		}
+		s++;
+	}
+	return res;
+}
+
+int romanToInt2(char* s) {
 	int res = 0;
 	int len = strlen(s);
 	char roman[] = {'I','V','X','L','C','D','M'};
