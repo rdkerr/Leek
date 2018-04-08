ecc2d7ec4b7060e36989cbd64c191ff858f66cd9
rdkerr <rogerdkerr@gmail.com>
2018-04-08 10:05:30 -0700

    Passed

--- Python3/Letter Combos of Phone Num/LetterCombosPhoneNum.py
+++ Python3/Letter Combos of Phone Num/LetterCombosPhoneNum.py
@@ -2,16 +2,25 @@
 class Solution:
     # 
     def letterCombinations(self, digits):
-        letters = {2:'abc',3:'def',4:'ghi',5:'jkl',6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
-        print(letters)
-        return []
+        if len(digits)==0:
+            return []
+        letters = {'2':'abc','3':'def','4':'ghi','5':'jkl',
+                   '6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        result = []
+        result.append('')
+        for digit in digits:
+            size = len(letters[digit])
+            temp = []
+            for res in result:
+                for letter in letters[digit]:
+                    temp.append(res + letter)
+            result = temp    
+        return result
     
 def main():
     test = Solution()
-    print(test.letterCombinations(23))
-    #print(test.letterCombinations(13))
-    #print(test.letterCombinations(131))
-    #print(test.letterCombinations(562))
+    print(test.letterCombinations('234'))
+
 
 
 if __name__ == "__main__":
