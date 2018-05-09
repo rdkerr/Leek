32b82c5ceb0636a4613540bc4403eae3d1856f93
rdkerr <rogerdkerr@gmail.com>
2018-05-09 12:41:17 -0700

    Completed Remove Nth Node From End Of List

--- Java/Remove Nth Node From End Of List/src/leet/Solution.java
+++ Java/Remove Nth Node From End Of List/src/leet/Solution.java
@@ -2,6 +2,38 @@ package leet;
 
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        ListNode ret = new ListNode(0);
+        ret.next = head;
+        ListNode temp = ret;
+        ListNode previous = ret;
+        int delay = 0 - n - 1;
+        while (temp.next != null) {
+        	temp = temp.next;
+        	delay++;
+        	if(delay >= 0) {
+        		previous = previous.next;
+        	}
+        }
+        ListNode target = previous.next;
+        previous.next = target.next;
+        return ret.next;
+    }
+    
+    public static void main(String[] args) {
+    	ListNode first = new ListNode(1);
+    	/**ListNode second = new ListNode(2);
+    	ListNode third = new ListNode(3);
+    	ListNode fourth = new ListNode(4);
+    	ListNode fifth = new ListNode(5);
+    	first.next = second;
+    	second.next = third;
+    	third.next = fourth;
+    	fourth.next = fifth;*/
+    	Solution sol = new Solution();
+    	ListNode tmp = sol.removeNthFromEnd(first,1);;
+    	while(tmp != null) {
+    		System.out.print(tmp.val);
+    		tmp = tmp.next;
+    	}
     }
 }
