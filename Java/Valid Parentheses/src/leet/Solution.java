05ce33ddaffd4c8d3525bc850a95788f7f07c6de
rdkerr <rogerdkerr@gmail.com>
2018-05-09 13:11:18 -0700

    Valid Parenthese passed but slow

--- Java/Valid Parentheses/src/leet/Solution.java
+++ Java/Valid Parentheses/src/leet/Solution.java
@@ -1,9 +1,40 @@
 package leet;
 
-public class Solution {
+import java.util.Stack;
 
+class Solution {
+    public boolean isValid(String s) {
+    	Stack<String> parentheses = new Stack<>();
+    	for(int i = 0 ; i < s.length() ; i ++) {
+    		String current = s.substring(i,i+1);
+    		if(current.equals("{")) {
+    			parentheses.push("}");
+    		}
+    		else if(current.equals("[")) {
+    			parentheses.push("]");
+    		}
+    		else if(current.equals("(")) {
+    			parentheses.push(")");
+    		}
+    		else {
+    			if(current.equals(parentheses.peek())) {
+    				parentheses.pop();
+    			}
+    			else {
+    				return false;
+    			}
+    		}
+    	}
+        return parentheses.isEmpty();
+    }
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+		System.out.println(sol.isValid("()"));
+		System.out.println(sol.isValid("()[]{}"));
+		System.out.println(sol.isValid("(]"));
+		System.out.println(sol.isValid("([)]"));
+		System.out.println(sol.isValid("{[]}"));
 
 	}
 
