de1b86ba8721e1ed6f6bc103487080c2931728a7
rdkerr <rogerdkerr@gmail.com>
2018-05-09 13:19:31 -0700

    init Merge Two Sorted Lists

---
+++ Java/Merge Two Sorted Lists/src/leet/Solution.java
@@ -0,0 +1,14 @@
+package leet;
+
+public class Solution {
+	
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
