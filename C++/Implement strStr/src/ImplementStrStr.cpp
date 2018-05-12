3f4004d2b2600f8b504ea3a62cf4fdae7d65c246
rdkerr <rogerdkerr@gmail.com>
2018-05-12 10:18:34 -0700

    init Implement strStr

---
+++ C++/Implement strStr/src/ImplementStrStr.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ImplementStrStr.cpp
+// Author      : Roger Kerr
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
