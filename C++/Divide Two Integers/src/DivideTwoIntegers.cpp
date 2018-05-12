58d1aac36f52819773e8252723638ef03cd195ca
rdkerr <rogerdkerr@gmail.com>
2018-05-12 16:14:11 -0700

    Timed out

--- C++/Divide Two Integers/src/DivideTwoIntegers.cpp
+++ C++/Divide Two Integers/src/DivideTwoIntegers.cpp
@@ -7,17 +7,45 @@
 //============================================================================
 
 #include <iostream>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
     static int divide(int dividend, int divisor) {
-        return 1;
+    	int result = 0;
+    	int sign = 0;
+    	if (dividend < 0) {
+    		sign++;
+    		dividend = 0 - dividend;
+    		if (dividend < 0) {
+    			return INT_MAX - 1;
+    		}
+    	}
+    	if (divisor < 0) {
+    		sign++;
+    		divisor = 0 - divisor;
+    	}
+    	int total = divisor;
+    	cout << (divisor << 1);
+    	while (total <= dividend) {
+    		total += divisor;
+    		result++;
+    		if (total < divisor) {
+    			return INT_MAX -1;
+    		}
+    	}
+    	if (sign != 1) {
+    		return result;
+    	}
+    	else {
+    		return 0 - result;
+    	}
     }
 };
 
 int main() {
-	int res = Solution::divide(7,3);
-	cout << res << endl; // prints !!!Hello World!!!
+	int res = Solution::divide(-112,-12);
+	//cout << res << endl;
 	return 0;
 }
