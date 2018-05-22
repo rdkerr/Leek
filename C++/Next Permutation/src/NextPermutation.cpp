6ef617d39425b7d43f099b233b27207bc7f369a2
rdkerr <rogerdkerr@gmail.com>
2018-05-22 13:06:33 -0700

    First attempt. Only solves simple cases

--- C++/Next Permutation/src/NextPermutation.cpp
+++ C++/Next Permutation/src/NextPermutation.cpp
@@ -12,16 +12,41 @@ using namespace std;
 
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-
+    static void nextPermutation(vector<int>& nums) {
+    	bool found = false;
+    	int i = nums.size();
+    	while(!found && i > 0) {
+    		int prev = i - 1;
+    		if (nums[prev] < nums[i]) {
+    			int temp = nums[prev];
+				nums[prev] = nums[i];
+				nums[i] = temp;
+				found = true;
+    		}
+    		i--;
+    	}
+    	if(!found) {
+    		int beg = 0;
+    		int end = nums.size()-1;
+    		while (beg < end) {
+    			int temp = nums[beg];
+				nums[beg] = nums[end];
+				nums[end] = temp;
+				beg++;
+				end--;
+    		}
+    	}
     }
 };
 
 int main() {
 	vector<int> nums;
-	nums.push_back(1);
-	nums.push_back(2);
+	nums.push_back(5);
+	nums.push_back(4);
+	nums.push_back(7);
+	nums.push_back(5);
 	nums.push_back(3);
-	Solution::nextPermuation(nums);
+	nums.push_back(2);
+	Solution::nextPermutation(nums);
 	return 0;
 }
