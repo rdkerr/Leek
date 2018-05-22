52b318b4734220d20cc6d2f86db0bfe23f8a71e7
rdkerr <rogerdkerr@gmail.com>
2018-05-22 12:16:42 -0700

    Passed test case

--- C++/Substring with Concatenation of All Words/src/SubstringWithConcatenationOfAllWords.cpp
+++ C++/Substring with Concatenation of All Words/src/SubstringWithConcatenationOfAllWords.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <map>
 using namespace std;
 
 
@@ -15,6 +17,42 @@ class Solution {
 public:
     static vector<int> findSubstring(string s, vector<string>& words) {
     	vector<int> res;
+    	int length = s.length();
+    	int wordCount = words.size();
+    	int wordLength = words[0].size();
+    	if (!length || !wordCount) {
+    		return res;
+    	}
+    	map<string,int> wordMap;
+    	for(string word:words) {
+    		wordMap[word]++;
+    	}
+		for(int windowStart=0; windowStart<wordLength; windowStart++){
+			unsigned int windowStartTemp = windowStart;
+			map<string,int> tempMap;
+			int tempCount = 0;
+			// Slide the window of wordLength
+			for(unsigned int i=windowStart; i<s.size(); i+=wordLength){
+				string str = s.substr(i, wordLength);
+				tempMap[str]++;
+				tempCount++;
+				// Remove words added to temp map, concatenated words not found
+				while(tempMap[str]>wordMap[str] && windowStartTemp<=i){
+					string lstr = s.substr(windowStartTemp,wordLength);
+					tempMap[lstr]--;
+					tempCount--;
+					windowStartTemp+=wordLength;
+				}
+				// Add starting index if all words found
+				if(tempCount==wordCount){
+					res.push_back(windowStartTemp);
+					string lstr = s.substr(windowStartTemp,wordLength);
+					tempMap[lstr]--;
+					tempCount--;
+					windowStartTemp+=wordLength;
+				}
+			}
+		}
         return res;
     }
 };
@@ -24,7 +62,7 @@ int main() {
 	vector<string> words;
 	words.push_back("foo");
 	words.push_back("bar");
-	string s = "foobartheboofar";
+	string s = "foobarthebarfoofooobar";
 	vector<int> ans = Solution::findSubstring(s,words);
 	for (auto a : ans) {
 		cout << a << "\n";
