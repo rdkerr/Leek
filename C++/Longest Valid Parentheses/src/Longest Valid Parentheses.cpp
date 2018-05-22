021188ca10f68344b6f0c4ab8ce6e532ef163dab
rdkerr <rogerdkerr@gmail.com>
2018-05-22 15:17:39 -0700

    Init Longeset Valid Parentheses

---
+++ C++/Longest Valid Parentheses/src/Longest Valid Parentheses.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Longest.cpp
+// Author      : Roger Kerr
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    static int longestValidParentheses(string s) {
+        return 0;
+    }
+};
+
+int main() {
+	int ans = Solution::longestValidParentheses("test");
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
