5fdb7a146246f8a56aa0cae4bf897b22c67c504b
rdkerr <rogerdkerr@gmail.com>
2018-06-03 14:38:08 -0700

    init Count And Say

---
+++ C++/Count And Say/src/CountAndSay.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : CountAndSay.cpp
+// Author      : Roger Kerr
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    static string countAndSay(int n) {
+        return "";
+    }
+};
+
+int main() {
+	string ans = Solution::countAndSay(5);
+	cout << ans << endl;
+	return 0;
+}
