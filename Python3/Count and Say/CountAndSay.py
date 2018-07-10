23d97b07f59c2f6bad91e96aef07555e8f69251b
rdkerr <rogerdkerr@gmail.com>
2018-07-10 12:07:57 -0700

    Skipping, question is confusing

--- Python3/Count and Say/CountAndSay.py
+++ Python3/Count and Say/CountAndSay.py
@@ -17,7 +17,21 @@ class Solution:
         :type n: int
         :rtype: str
         """
-        return n
+        s = '1'
+        for i in range(n-1):
+            count = 1
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s
         
         
 def main():
