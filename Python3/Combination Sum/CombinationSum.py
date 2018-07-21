2f92a13721c2efb05d0fa4bdc0211f5670174390
rdkerr <rogerdkerr@gmail.com>
2018-07-21 13:06:09 -0700

    Solution accepted

--- Python3/Combination Sum/CombinationSum.py
+++ Python3/Combination Sum/CombinationSum.py
@@ -27,30 +27,30 @@ class Solution:
         :rtype: List[List[int]]
         """
         self.result = set()
-        self.candidates = candidates
+        self.candidates = candidates.sort()
         self.target = target
-        self.search()
-        return list(result)
-
-    def sub(self, cand):
-        res =[]
-        self.back(res,[],cand,0)
-        return res
+        self.back([],candidates,0,0)
+        return [[element for element in tup] for tup in self.result]
     
-    def back(self, res, temp, cand, start):
-        res.append(list(temp))
-        print(res,temp,start)
-        for i in range(start,len(cand)):
-            temp.append(cand[i])
-            self.back(res,temp,cand,i+1)
-            temp.pop()
+    def back(self, temp, cand, start,total):
+        if total==self.target:
+            self.result.add(tuple(temp))
+        elif total > self.target:
+                return
+        else:
+            for i in range(start,len(cand)):
+                temp.append(cand[i])
+                print(temp)
+                newTotal = total + cand[i]
+                self.back(temp,cand,i,newTotal)
+                temp.pop()
+                newTotal = newTotal - cand[i]
 
 def main():
     sol = Solution()
-    candidates = [2,3,5]
+    candidates = [2,3,5,6]
     target = 8
-    print(sol.sub(candidates))
-    #print(sol.combinationSum(candidates,target))
+    print(sol.combinationSum(candidates,target))
        
 
 if __name__ == "__main__":
