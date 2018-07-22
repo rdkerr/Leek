dde98de9fd82c01e7a04fc1a4ba836cfc987138c
rdkerr <rogerdkerr@gmail.com>
2018-07-21 17:11:02 -0700

    Accepted Trapping Rain Water

--- Javascript/Trapping Rain Water/src/TrappingRainWater.js
+++ Javascript/Trapping Rain Water/src/TrappingRainWater.js
@@ -3,5 +3,30 @@
  * @return {number}
  */
 var trap = function(height) {
-    return 0;
+	var left = 0
+	var right = height.length-1;
+	var leftMax = 0;
+	var rightMax = 0;
+	var total = 0;
+	while(left < right) {
+		if (height[left] < height[right]) {
+			if (height[left] >= leftMax) {
+				leftMax = height[left];
+			}
+			else {
+				total +=  (leftMax - height[left]);
+			}
+			left ++;
+		}
+		else {
+			if (height[right] >= rightMax) {
+				rightMax = height[right];
+			}
+			else {
+				total += (rightMax - height[right]);
+			}
+			right--;
+		}
+	}
+    return total;
 };
