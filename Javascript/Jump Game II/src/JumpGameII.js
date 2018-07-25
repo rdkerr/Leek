45ad96a8f69042735eb6fba0bf1e4c49dd7cd637
rdkerr <rogerdkerr@gmail.com>
2018-07-25 15:38:17 -0700

    Accepted Jump Game II

--- Javascript/Jump Game II/src/JumpGameII.js
+++ Javascript/Jump Game II/src/JumpGameII.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var jump = function(nums) {
-    return 0;
+	var step = 0;
+	var end = 0; 
+	var reach = 0;
+	for(var i = 0 ; i < nums.length - 1; i++) {
+		reach = Math.max(reach, i + nums[i]);
+		if (i == end) {
+			step ++;
+			end = reach;
+		}
+	}
+    return step;
 };
