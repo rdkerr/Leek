d4830f4eeda7dbbfb887e1faf9c3f83174908b9b
rdkerr <rogerdkerr@gmail.com>
2018-10-01 14:19:21 -0700

    Init Unique Paths

---
+++ Python3/Unique Paths/Unique Paths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        return 0
+        
+def main():
+    sol = Solution()
+    print(sol.uniquePaths(3,3))
+    print(sol.uniquePaths(4,9))
+    print(sol.uniquePaths(5,3))
+
+if __name__ == "__main__":
+    main()
