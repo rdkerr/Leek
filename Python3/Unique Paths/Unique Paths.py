7fdf3b4150bb2c688d06170aa3f2cfc14105670a
rdkerr <rogerdkerr@gmail.com>
2018-10-01 14:36:48 -0700

    Accepted Unique Paths

--- Python3/Unique Paths/Unique Paths.py
+++ Python3/Unique Paths/Unique Paths.py
@@ -5,13 +5,19 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        return 0
+        paths = [[1 for col in range(m)] for row in range(n)]
+
+        print(paths,m-1,n-1)
+        for row in range(1,n):
+        	for col in range(1,m):
+        		paths[row][col] = paths[row-1][col] + paths[row][col-1]
+        return paths[n-1][m-1]
         
 def main():
     sol = Solution()
     print(sol.uniquePaths(3,3))
     print(sol.uniquePaths(4,9))
-    print(sol.uniquePaths(5,3))
+    print(sol.uniquePaths(7,3))
 
 if __name__ == "__main__":
     main()
