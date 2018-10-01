c38b65f800b62a163a2bb4639d148d29948fce2a
rdkerr <rogerdkerr@gmail.com>
2018-10-01 14:39:10 -0700

    Init Unique Paths II

---
+++ Python3/Unique Paths II/Unique Paths II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        return 0
+        
+def main():
+    sol = Solution()
+    obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+    print(sol.uniquePathsWithObstacles(obstacleGrid))
+
+if __name__ == "__main__":
+    main()
