26df9f2ddf0652964dcfd0cced24b8d32a45a12c
rdkerr <rogerdkerr@gmail.com>
2018-10-27 12:26:23 -0700

    Init Plus One

---
+++ Java/Plus One/src/leet/Solution.java
@@ -0,0 +1,13 @@
+package leet;
+
+public class Solution {
+	    public int[] plusOne(int[] digits) {
+	        return null;
+	    }
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.plusOne(new int[] {1,2,3}));
+		System.out.println(sol.plusOne(new int[] {4,3,2,1}));
+	}
+}
