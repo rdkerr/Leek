da7d60171bf37eccb245c55ef15a8a2fb78637ba
rdkerr <rogerdkerr@gmail.com>
2018-10-27 13:35:08 -0700

    Update Plus One
    Solution works but it fails on cases greater than INT_MAX

--- Java/Plus One/src/leet/Solution.java
+++ Java/Plus One/src/leet/Solution.java
@@ -1,13 +1,23 @@
 package leet;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Solution {
 	    public int[] plusOne(int[] digits) {
-	        return null;
+	    	String temp = Arrays.stream(digits)
+	    		.mapToObj(Integer::toString)
+	    		.collect(Collectors.joining(""));
+	    	String num = String.valueOf((Integer.parseInt(temp) + 1));
+	    	int[] arr = Arrays.stream(num.split(""))
+	    			.mapToInt(Integer::valueOf)
+	    			.toArray();
+	        return arr;
 	    }
 	
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		System.out.println(sol.plusOne(new int[] {1,2,3}));
-		System.out.println(sol.plusOne(new int[] {4,3,2,1}));
+		int[] t = sol.plusOne(new int[] {9,8,7,6,5,4,3,2});
+		for (int i : t) {System.out.println(i);}
 	}
 }
