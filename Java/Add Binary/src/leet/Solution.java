e0f2043c728ac4426743719dc04f2b4ef5b57a1a
rdkerr <rogerdkerr@gmail.com>
2018-10-27 14:03:58 -0700

    Init Add Binary

---
+++ Java/Add Binary/src/leet/Solution.java
@@ -0,0 +1,12 @@
+package leet;
+
+public class Solution {
+	public String addBinary(String a, String b) {
+		return null;
+    }
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.addBinary("1", "101"));
+	}
+}
