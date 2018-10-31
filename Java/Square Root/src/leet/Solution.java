564fd46c66dbf125cf410725ccf9c673fd171318
rdkerr <rogerdkerr@gmail.com>
2018-10-30 18:34:11 -0700

    Init Square Root

---
+++ Java/Square Root/src/leet/Solution.java
@@ -0,0 +1,16 @@
+package leet;
+
+public class Solution {
+	public int mySqrt(int x) {
+        return 0;
+    }
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.mySqrt(4));
+		System.out.println(sol.mySqrt(44));
+		System.out.println(sol.mySqrt(455));
+		System.out.println(sol.mySqrt(4666));
+		
+	}
+
+}
