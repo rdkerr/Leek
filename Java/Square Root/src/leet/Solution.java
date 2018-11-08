e13618d433ccd76cc3b222fbfdabd0145e4a337f
rdkerr <rogerdkerr@gmail.com>
2018-11-08 12:04:10 -0700

    Accepted Square Root

--- Java/Square Root/src/leet/Solution.java
+++ Java/Square Root/src/leet/Solution.java
@@ -2,7 +2,26 @@ package leet;
 
 public class Solution {
 	public int mySqrt(int x) {
-        return 0;
+		int ans = 0;
+		if (x <= 1) {
+			ans = x;
+		}
+		else {
+			int left = 0;
+			int right = x;
+			int mid = x;
+			while (left < right) {
+				mid = left + (right - left) / 2;
+				if (mid <= x / mid) {
+					left = mid + 1;
+				}
+				else {
+					right = mid;
+				}
+			}
+			ans = right - 1;
+		}
+        return ans;
     }
 	public static void main(String[] args) {
 		Solution sol = new Solution();
