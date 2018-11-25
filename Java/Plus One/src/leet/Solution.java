8db38f14c1a9ab90e490142abc33a1570339f231
rdkerr <rogerdkerr@gmail.com>
2018-11-24 20:22:25 -0700

    Remove unused libraries

--- Java/Plus One/src/leet/Solution.java
+++ Java/Plus One/src/leet/Solution.java
@@ -1,57 +1,53 @@
 package leet;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+//import java.util.Arrays;
+//import java.util.stream.Collectors;
 import java.util.ArrayList;
 
 public class Solution {
-	    public int[] plusOne(int[] digits) {
-	    	/**
-	    	String temp = Arrays.stream(digits)
-	    		.mapToObj(Integer::toString)
-	    		.collect(Collectors.joining(""));
-	    	String num = String.valueOf((Integer.parseInt(temp) + 1));
-	    	int[] arr = Arrays.stream(num.split(""))
-	    			.mapToInt(Integer::valueOf)
-	    			.toArray();
-	    			*/
-	    	int carry = (digits[digits.length-1] == 9) ? 1 : 0;
-	    	ArrayList<Integer> list = new ArrayList<Integer>();
-	    	if (carry == 1) {
-	    		list.add(0);
-	    	}
-	    	else {
-	    		list.add(digits[digits.length-1]+1);
-	    	}
-	    	for (int i = digits.length - 2 ; i >= 0 ; i --) {
-	    		int current = digits[i];
-	    		if (carry > 0 && current == 9) {
-	    			list.add(0,0);
-	    			carry = 1;
-	    		}
-	    		else if (carry > 0) {
-	    			list.add(0,current + 1);
-	    			carry = 0;
-	    		}
-	    		else {
-	    			list.add(0,current);
-	    			carry = 0;
-	    		}
-	    	}
-	    	if (carry > 0) {
-	    		list.add(0,1);
-	    	}
-	    	int[] arr = new int[list.size()];
-	    	int index = 0;
-	    	for (Integer num : list) {
-	    		arr[index++] = num;
-	    	}
-	        return arr;
-	    }
-	
+	public int[] plusOne(int[] digits) {
+		/**
+		 * String temp = Arrays.stream(digits) .mapToObj(Integer::toString)
+		 * .collect(Collectors.joining("")); String num =
+		 * String.valueOf((Integer.parseInt(temp) + 1)); int[] arr =
+		 * Arrays.stream(num.split("")) .mapToInt(Integer::valueOf) .toArray();
+		 */
+		int carry = (digits[digits.length - 1] == 9) ? 1 : 0;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if (carry == 1) {
+			list.add(0);
+		} else {
+			list.add(digits[digits.length - 1] + 1);
+		}
+		for (int i = digits.length - 2; i >= 0; i--) {
+			int current = digits[i];
+			if (carry > 0 && current == 9) {
+				list.add(0, 0);
+				carry = 1;
+			} else if (carry > 0) {
+				list.add(0, current + 1);
+				carry = 0;
+			} else {
+				list.add(0, current);
+				carry = 0;
+			}
+		}
+		if (carry > 0) {
+			list.add(0, 1);
+		}
+		int[] arr = new int[list.size()];
+		int index = 0;
+		for (Integer num : list) {
+			arr[index++] = num;
+		}
+		return arr;
+	}
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		int[] t = sol.plusOne(new int[] {9,8,7,6,5,4,3,2});
-		for (int i : t) {System.out.println(i);}
+		int[] t = sol.plusOne(new int[] { 9, 8, 7, 6, 5, 4, 3, 2 });
+		for (int i : t) {
+			System.out.println(i);
+		}
 	}
 }
--- Java/Square Root/src/leet/Solution.java
+++ Java/Square Root/src/leet/Solution.java
@@ -1,15 +1,14 @@
 package leet;
 
-import java.util.Arrays;
-import java.util.List;
+//import java.util.Arrays;
+//import java.util.List;
 
 public class Solution {
 	public int mySqrt(int x) {
 		int ans = 0;
 		if (x <= 1) {
 			ans = x;
-		}
-		else {
+		} else {
 			int left = 0;
 			int right = x;
 			int mid = x;
@@ -17,22 +16,22 @@ public class Solution {
 				mid = left + (right - left) / 2;
 				if (mid <= x / mid) {
 					left = mid + 1;
-				}
-				else {
+				} else {
 					right = mid;
 				}
 			}
 			ans = right - 1;
 		}
-        return ans;
-    }
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		System.out.println(sol.mySqrt(4));
 		System.out.println(sol.mySqrt(44));
 		System.out.println(sol.mySqrt(455));
 		System.out.println(sol.mySqrt(4666));
-		
+
 	}
 
 }
