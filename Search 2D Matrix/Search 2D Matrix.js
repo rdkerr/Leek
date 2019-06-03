352ad45137fe685592189750667b8bc5e7b4dcf0
rdkerr <rogerdkerr@gmail.com>
2019-06-03 12:15:57 -0700

    Accepted Search 2D Matrix

--- Search 2D Matrix/Search 2D Matrix.js
+++ Search 2D Matrix/Search 2D Matrix.js
@@ -4,34 +4,36 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let col = matrix.length - 1;
-    let row = 0;
+    let row = matrix.length - 1;
+    let col = 0;
     if (matrix.length === 0) return false;
-    while (matrix[row][0] && matrix[row][0] < target) row++;
-    row--;
-    while (col > 0) {
-        if (matrix[row-1][col] === target) {
+    while (matrix[row] && matrix[row][0] && matrix[row][0] > target) row--;
+    if (row < 0) return false;
+    while (col < matrix[row].length) {
+        if (matrix[row][col] === target) {
             return true;
         }
-        col--;
+        col++;
     }
     return false;
 };
 
 // Input:
-// matrix = [
-//   [1,   3,  5,  7],
-//   [10, 11, 16, 20],
-//   [23, 30, 34, 50]
-// ]
-// target = 3
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+console.log(searchMatrix(matrix, target))
 // Output: true
 
 // Input:
-// matrix = [
-//   [1,   3,  5,  7],
-//   [10, 11, 16, 20],
-//   [23, 30, 34, 50]
-// ]
-// target = 13
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+console.log(searchMatrix(matrix, target))
 // Output: false
